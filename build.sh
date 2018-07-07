2efbb14be2b30fbb6866d324b0459dd7730c5cd8
haolun <haolun@example.com>
2018-07-07 19:14:22 +0800

    fix image name

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ WORK_DIR=$(pwd)
 if [ -n "${CI_OPT_DOCKER_REGISTRY_PASS}" ] && [ -n "${CI_OPT_DOCKER_REGISTRY_USER}" ]; then echo ${CI_OPT_DOCKER_REGISTRY_PASS} | docker login --password-stdin -u="${CI_OPT_DOCKER_REGISTRY_USER}" docker.io; fi
 
 export IMAGE_PREFIX=${IMAGE_PREFIX:-cirepo};
-export IMAGE_NAME=${IMAGE_NAME:-rust}
+export IMAGE_NAME=${IMAGE_NAME:-bionic-rust}
 export IMAGE_TAG=${IMAGE_ARG_RUST_CHANNEL:-stable}
 if [ "${TRAVIS_BRANCH}" != "master" ]; then export IMAGE_TAG=${IMAGE_TAG}-SNAPSHOT; fi
 
